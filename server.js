36745a952360a22070774de371bc0a9c3d8bfa00
Jose Cerqueira <jose@origensmedia.com>
2019-07-25 15:30:32 +0100

    connected our app to a cloud mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import './env';
+import mongoose from 'mongoose';
 import app from './app';
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 8080;
 
